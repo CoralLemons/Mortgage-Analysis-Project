c54d5122fe8b89783d95c30ea3bfa4c711c240ae
OBSIDIAN\lwarren <developer@warrencode.com>
2024-04-04 12:57:01 -0600

    basic outline for discussion, added income for algo filter with basic validation

---
+++ Code/sandbox_algo.py
@@ -0,0 +1,9 @@
+
+## outline for algo logic/UI update
+# 1) grab data filter from each drop down
+# 2) grab data from income text box
+# 3) filter data from (1) with income range from (2)
+# 4) populate the counties with the filtered data for mouseover event
+# 5) populate the tool tip with % success and other relevant data
+## end outline
+
