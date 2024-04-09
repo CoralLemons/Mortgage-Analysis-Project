f11c5f35dfe06274d1da80e36c91950bc00da9d5
Shayla Nelson <xelamarie@gmail.com>
2024-04-08 21:30:51 -0600

    Fill in functionality

--- docs/main.js
+++ docs/main.js
@@ -6,10 +6,16 @@ const svg = d3.select("#chart");
 const svgChartEl = document.getElementById("chart");
 const resultsBtn = document.getElementById("results-button");
 
-const lgrCoefs = d3.csv("lgrcoef.csv");
+const codeToState = {
+  48: "TX",
+  53: "WA",
+  36: "NY",
+};
 
-d3.json("https://d3js.org/us-10m.v2.json").then(function (usData) {
-  function drawState(selectedStateId) {
+d3.json("https://d3js.org/us-10m.v2.json").then(async function (usData) {
+  const lgrCoefs = await d3.csv("lgrcoef.csv");
+
+  function drawState(selectedStateId, coefs) {
     svgChartEl.innerHTML = "";
 
     const stateData = topojson
@@ -20,7 +26,29 @@ d3.json("https://d3js.org/us-10m.v2.json").then(function (usData) {
       .feature(usData, usData.objects.counties)
       .features.filter((d) => d.id.indexOf(selectedStateId) === 0);
 
-    console.log(usData.objects.states);
+    // console.log(countiesData);
+    coefs.counties = countiesData.map((x) => ({
+      id: x.id,
+      name: x.properties.name,
+      value: getFeatureCoef("county_code_", x.id),
+    }));
+
+    // console.log(coefs);
+    const countyPredictions = coefs.counties.map((x) => {
+      return {
+        prediction:
+          (coefs.intercept + // intercept
+            x.value + // county coef
+            Math.log(coefs.income) * coefs.incomeLog + // income
+            coefs.ageCoef + // age
+            coefs.oTypeCoef) / // occupancy type
+          10, // TODO this is fake, just to make the numbers look good for now
+        ...x,
+      };
+    });
+    updateTable(countyPredictions);
+
+    // console.log(usData.objects.states);
 
     const projection = d3.geoIdentity().fitSize([width, height], stateData[0]);
 
@@ -71,9 +99,70 @@ d3.json("https://d3js.org/us-10m.v2.json").then(function (usData) {
       );
   }
 
+  function getFeatureCoef(prefix, value) {
+    const key = `${prefix}${value}`;
+    const coef = lgrCoefs.find((x) => x.Feature === key);
+    return parseFloat(coef.Coefficient);
+  }
+
+  function updateTable(countyPredictions) {
+    const sorted = countyPredictions.sort(
+      (a, b) => b.prediction - a.prediction
+    );
+    const limited = sorted.slice(0, 10);
+    const tableBody = document.querySelector("#top-10 > tbody");
+    tableBody.innerHTML = "";
+    limited.forEach((x, idx) => {
+      const row = document.createElement("tr");
+
+      const rankCol = document.createElement("td");
+      rankCol.innerHTML = idx + 1;
+      row.appendChild(rankCol);
+
+      const nameCol = document.createElement("td");
+      nameCol.innerHTML = x.name;
+      row.appendChild(nameCol);
+
+      const probCol = document.createElement("td");
+      probCol.innerHTML = x.prediction.toFixed(4);
+      row.appendChild(probCol);
+
+      tableBody.appendChild(row);
+    });
+  }
+
   function renderData() {
+    // console.log(lgrCoefs);
+
     const state = document.getElementById("state-select").value;
-    drawState(state);
+    const stateCoef = getFeatureCoef("state_code_", codeToState[state]);
+    console.log(`coef for ${codeToState[state]}:`, stateCoef);
+
+    // TODO need applicant_sex instead of derived gender
+    // const gender = document.getElementById("gender-select").value;
+    // const genderCoef = getFeatureCoef("applicant_sex", gender);
+    // console.log(`coef for ${gender}:`, genderCoef);
+
+    const age = document.getElementById("age-select").value;
+    const ageCoef = getFeatureCoef("applicant_age_", age);
+    console.log(`coef for ${age}:`, ageCoef);
+
+    // TODO way too many options need to consolidate
+    // const race = document.getElementById("race-select").value;
+    // const raceCoef = getFeatureCoef("race_ethnicity", race);
+    // console.log(`coef for ${race}:`, raceCoef);
+
+    const oType = document.getElementById("occupancy_type-select").value;
+    const oTypeCoef = getFeatureCoef("occupancy_type_", oType);
+    console.log(`coef for ${oType}:`, oTypeCoef);
+
+    const income = document.getElementById("income-entry").value;
+    const intercept = getFeatureCoef("Intercept", "");
+    const incomeLog = getFeatureCoef("income_log", "");
+
+    coefs = { stateCoef, ageCoef, oTypeCoef, income, incomeLog, intercept };
+
+    drawState(state, coefs);
   }
 
   resultsBtn.addEventListener("click", (evt) => {
